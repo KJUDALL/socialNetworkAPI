624c2344025b411efaf03d8e49e0eeda125fb249
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-12-29 15:18:55 -0500

    finalized User.ts and Thought.ts with the Reaction schema included

--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -30,7 +30,7 @@ const reactionSchema = new Schema(
 );
 
 interface IReaction extends Document {
-	reactionID: Types.ObjectId;
+	reactionId: Types.ObjectId;
 	reactionBody: string;
 	username: string;
 	createdAt: Date;
@@ -42,3 +42,29 @@ interface IThought extends Document {
 	username: string;
 	reactions: IReaction[];
 }
+
+const thoughtSchema = new Schema<IThought>({
+	thoughtText: {
+		type: String,
+		required: true,
+		maxlength: 280,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	reactions: [reactionSchema],
+});
+
+//retrieve length of thought's reaction array
+thoughtSchema.virtual('reactionCount').get(function (this: IThought) {
+    return this.reactions.length;
+});
+    
+const Thought = model<IThought>("Thought", thoughtSchema);
+
+export default Thought;
