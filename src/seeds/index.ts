6d36a7e73b006da310201f0660af1fc4bdde1024
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2025-01-01 18:33:26 -0500

    updated DB naming convention to match what I have created in mongoose.

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDb = async () => {
 	try {
-		await mongoose.connect("mongodb://localhost:27017/social_network");
+		await mongoose.connect("mongodb://localhost:27017/socialNetworkAPI");
 		console.log("Connected to DB successfully!");
 	} catch (err) {
 		console.error("Failed to connect to DB.", err);
